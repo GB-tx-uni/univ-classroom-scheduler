90e24e972df6426d51059d88ea379f8784fdf959
Ekta <ekta.k@gobasktusa.com>
2023-02-01 11:58:27 +0530

    modified css

--- layout/Professor.tsx
+++ layout/Professor.tsx
@@ -6,7 +6,7 @@ const Professor = ({ children }: { children: any }) => {
     return (
         <>
             <Sidebar />
-            <div className="md:ml-64 bg-blueGray-100">
+            <div className="bg-blueGray-100">
                 <AdminNavbar />
                 <div className="px-4 md:px-10 mx-auto" style={{ marginLeft: '16rem', paddingTop: '100px' }}>
                     {children}
